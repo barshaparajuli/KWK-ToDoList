3c603f67ac500ec334befbf5b038000a23655a18
barshaparajuli <40861964+barshaparajuli@users.noreply.github.com>
2021-07-20 10:54:57 -0400

    Add files via upload
    Iteration One

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Barsha Parajuli on 7/20/21.
+//
+
+import UIKit
+
+class ToDoClass{
+    var description = ""
+    var important = false
+}
---
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Barsha Parajuli on 7/20/21.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var listOfToDo : [ToDoClass] = []
+    func createToDo() -> [ToDoClass]{
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let decorToDo = ToDoClass()
+        decorToDo.description = "Decorate Room"
+        
+        return[swiftToDo, decorToDo]
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        listOfToDo = createToDo()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let eachToDo = listOfToDo[indexPath.row]
+        if eachToDo.important{
+            cell.textLabel?.text = "❗️" + eachToDo.description
+        }else{
+            cell.textLabel?.text = "❕" + eachToDo.description
+        }
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
